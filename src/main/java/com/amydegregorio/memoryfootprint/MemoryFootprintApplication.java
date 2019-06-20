39e3513455328d3359fa4d8280b813431a84b918
amdegregorio <amy.m.degregorio@gmail.com>
2019-06-19 20:29:40 -0400

    initial vanilla code

---
+++ src/main/java/com/amydegregorio/memoryfootprint/MemoryFootprintApplication.java
@@ -0,0 +1,13 @@
+package com.amydegregorio.memoryfootprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryFootprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryFootprintApplication.class, args);
+	}
+
+}
